05c048546335b9add8c018f00738a08df0a52137
ManuelMolino <mmolmil158@g.educaand.es>
2025-01-20 14:41:33 +0100

    Ejercicios de composición

--- src/main/java/org/example/ejercicio3/Producto.java
+++ src/main/java/org/example/ejercicio3/Producto.java
@@ -1,5 +1,25 @@
 package org.example.ejercicio3;
 
+import java.util.Objects;
+
 public record Producto(int id, String nombreProducto, double precio) {
-    
+    //1,gafas de solo,125.20
+
+    @Override
+    public String toString() {
+        return String.format("%d;%s;%.2f", id, nombreProducto, precio).
+                replace(',', '.').replace(';',',');
+    }
+
+   /* @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return id == producto.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }*/
 }
